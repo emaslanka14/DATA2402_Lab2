57fe5cf5defb928af2568b31f2099c025285a2fb
ColeWas <cwasm462@mtroyal.ca>
2026-09-24 15:13:37 -0600

    broke down logic into some more reusable functions and wrote more test cases

--- main1.py
+++ main1.py
@@ -14,7 +14,7 @@ def remove_stopwords(filename: str, stopwords: set) -> list:
     except FileNotFoundError:
         print(f"Error: file not found: {filename}")
         return []
-    translator = str.maketrans('', '', string.punctuation)
+    translator = str.maketrans('', '', string.punctuation) #Thanks to https://www.geeksforgeeks.org/python/python-remove-punctuation-from-string/#google_vignette
     cleanedText = str(text).translate(translator)
     newList = cleanedText.strip().split()
 
@@ -43,9 +43,44 @@ def get_term_freq(words: list) -> dict:
     frequency = {}
     total = sum(occurrences.values())
     for w in occurrences:
-        frequency[w] = occurrences[w] / total
+        try:
+            frequency[w] = occurrences[w] / total
+        except ValueError:
+            print(f"Value error for key: f{w}")
+            frequency[w] = None
+
+
     return frequency
-        
+
+def remove_common_words(dictA: dict, dictB: dict) -> None:
+    common = dictA.keys() & dictB.keys() 
+    print(f'common words set: {common}')
+
+    for w in common:
+        dictA.pop(w)
+        dictB.pop(w)
+    return None
+
+def word_freq_from_text_file(fileNameA:str, fileNameB:str=None) -> None:
+    stopwords = load_stopwords("stopwords.txt")   # returns a set
+    words_a = remove_stopwords(fileNameA, stopwords)
+    freq_A = get_term_freq(words_a)
+
+    if fileNameB: 
+        words_b = remove_stopwords("textB.txt", stopwords)         
+        freq_B = get_term_freq(words_b)
+
+        remove_common_words(freq_A, freq_B)
+
+    print_dict(freq_A)
+    if fileNameB:
+        print_dict(freq_B)
+
+def print_dict(myDict: dict):
+    for key, value in myDict.items():
+        print(f"Word: {key} Frequency {value}")
+    
+
 
 def main():
     stopwords = load_stopwords("stopwords.txt")   # returns a set
@@ -64,16 +99,11 @@ def main():
     print(freq_A)
     print(freq_B)
 
-    common = freq_A.keys() & freq_B.keys() 
-    print(common)
+    remove_common_words(freq_A, freq_B)
 
-    for w in common:
-        freq_A.pop(w)
-        freq_B.pop(w)
-
-    print(list(freq_A.keys()))
-    print(list(freq_B.keys()))
+    print_dict(freq_A)
+    print_dict(freq_B)
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": #Only run this if we are running this file itself
     main()
